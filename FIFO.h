b70a870616a37fb83bc0dae5ae29aba9020fac22
abau2002 <147750789+abau2002@users.noreply.github.com>
2023-11-16 15:59:38 -0500

    Update FIFO.h

--- FIFO.h
+++ FIFO.h
@@ -2,7 +2,7 @@
 // A Bautista, B Franco, E Mora
 // OS, Fall 2023, Transy U
 //
-// 
+// Interface for FIFO that contains all functions needed to page a process using the FIFO algorithm
 //
 
 #ifndef FIFO_H
@@ -28,7 +28,7 @@ private:
   int pageFaults;
   queue<int> pages;
   
-  // returns the first frame in the frames queue
+  // returns the first page in the pages queue
   int selectVictim();
 };
 
