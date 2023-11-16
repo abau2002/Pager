ac52c78e8eb8c7d9f5da10f2e45a17b29c61d2a8
abau2002 <147750789+abau2002@users.noreply.github.com>
2023-11-16 16:00:39 -0500

    Update LRU.cpp

--- LRU.cpp
+++ LRU.cpp
@@ -2,7 +2,7 @@
 // A Bautista, B Franco, E Mora
 // OS, Fall 2023, Transy U
 //
-// 
+// Implementation for LRU that contains all functions needed to page a process using the LRU algorithm
 //
 
 #include "Pager.h"
